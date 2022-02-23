e8e9a592d7ceb66ae5e3afa4207fe7c3b5d62bb8
asemin08 <semin.allan@gmail.com>
2022-02-23 12:12:00 +0100

    Ajout tp10

---
+++ src/main/java/eu/ensup/allan/demowsrest/service/JerseyService.java
@@ -0,0 +1,14 @@
+package eu.ensup.allan.demowsrest.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/message")
+public class JerseyService
+{
+    @GET
+    public String getMsg()
+    {
+        return "Hello World !! - Jersey 2";
+    }
+}
